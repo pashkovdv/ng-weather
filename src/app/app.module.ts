122fa1a938a14f7c7b8c0a697840544101d8b8c1
pashkovdv <pashkovdv@mail.ru>
2020-10-27 18:43:09 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewsComponent } from './news/news.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NewsComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: NewsComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: '404', component: NotFoundComponent},
+      {path: '**', redirectTo: '/404'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
